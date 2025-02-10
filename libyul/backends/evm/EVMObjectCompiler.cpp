5e125ad3cffad6746a5ab909acad8a9e53cf20be
clonker <1685266+clonker@users.noreply.github.com>
2025-02-10 09:38:48 +0100

    Yul FunctionCallFinder uses FunctionHandle instead of strings as search key

--- libyul/backends/evm/EVMObjectCompiler.cpp
+++ libyul/backends/evm/EVMObjectCompiler.cpp
@@ -93,10 +93,11 @@ void EVMObjectCompiler::run(Object const& _object, bool _optimize)
 		if (!stackErrors.empty())
 		{
 			yulAssert(_object.dialect());
+			auto const memoryGuardHandle = evmDialect->findBuiltin("memoryguard");
+			yulAssert(memoryGuardHandle, "Compiling with object access, memoryguard should be available as builtin.");
 			std::vector<FunctionCall const*> memoryGuardCalls = findFunctionCalls(
 				_object.code()->root(),
-				"memoryguard",
-				*_object.dialect()
+				*memoryGuardHandle
 			);
 			auto stackError = stackErrors.front();
 			std::string msg = stackError.comment() ? *stackError.comment() : "";
